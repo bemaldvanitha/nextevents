00aa13280d56fe8c1e5433e82ad1d51ea488e098
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-09 15:09:55 +0530

    event detail page finish

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,8 +1,30 @@
+import { useRouter } from 'next/router';
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+import { getEventById } from '../../dummy-data';
+
 const EventDetailPage = () => {
+    const router = useRouter();
+
+    const eventId = router.query.id;
+    const selectedEvent = getEventById(eventId);
+
+    if(!selectedEvent){
+        return <p>No event found</p>
+    }
+
     return(
-        <div>
-            <h1>Selected event page</h1>
-        </div>
+        <>
+            <EventSummary title={ selectedEvent.title }/>
+            <EventLogistics date={ selectedEvent.date } address={ selectedEvent.location }
+                image={ selectedEvent.image } imageAlt={ selectedEvent.title }/>
+            <EventContent>
+                <p>{ selectedEvent.description }</p>
+            </EventContent>
+        </>
     )
 }
 
