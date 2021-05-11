2b0f4ad6b6b5cadec19349c13f3a76de24807289
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-11 14:40:38 +0530

    redirect feacher added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,22 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
+    const router = useRouter();
     const events = getAllEvents();
+
+    const findEventsHandler = (year, month) => {
+        const path = `/events/${year}/${month}`;
+        router.push(path);
+    }
 
     return(
         <>
-            <EventsSearch/>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList events={events}/>
         </>
     )
