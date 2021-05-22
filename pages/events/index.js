991e1114a74fefd63d96cf282103b418d0acb718
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-05-22 14:55:50 +0530

    all events page staticly generated

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,14 @@
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
+// import { getAllEvents } from '../../dummy-data';
 
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-const AllEventsPage = () => {
+const AllEventsPage = (props) => {
     const router = useRouter();
-    const events = getAllEvents();
+    const events = props.events;
 
     const findEventsHandler = (year, month) => {
         const path = `/events/${year}/${month}`;
@@ -20,6 +21,17 @@ const AllEventsPage = () => {
             <EventList events={events}/>
         </>
     )
+}
+
+export async function getStaticProps(context){
+   const allEvents = await getAllEvents();
+
+   return {
+       props: {
+           events: allEvents
+       },
+       revalidate: 600
+   }
 }
 
 export default AllEventsPage;
