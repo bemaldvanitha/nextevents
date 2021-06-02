a047c73117655ec7e85b512586752af9b9d49c0d
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-02 14:14:42 +0530

    connect to mongodb

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,4 +1,6 @@
-const handler = (req,res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req,res) => {
 
     if(req.method === 'POST'){
         const { email } = req.body;
@@ -6,6 +8,15 @@ const handler = (req,res) => {
         if(!email  || !email.includes('@')){
             return res.status(422).json({ message: 'invalid email' });
         }
+
+        const client = await MongoClient.connect('mongodb+srv://bemaldvanitha:Bemal123@devconnector.2jyrs.mongodb.net/nextevents?retryWrites=true&w=majority');
+        const db = client.db();
+
+        await db.collection('emails').insertOne({
+            email: email
+        });
+
+        await client.close();
 
         return res.status(201).json({ message: 'sign up' });
     }
