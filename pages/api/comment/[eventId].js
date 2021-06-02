b36453275168778f1977de7b7db5153cbdbe1fa2
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-02 14:23:15 +0530

    comments added to mongo db

--- pages/api/comment/[eventId].js
+++ pages/api/comment/[eventId].js
@@ -1,5 +1,10 @@
-const handler = (req,res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req,res) => {
     const eventId = req.query.eventId;
+
+    const client = await MongoClient.connect('mongodb+srv://bemaldvanitha:Bemal123@devconnector.2jyrs.mongodb.net/nextevents?retryWrites=true&w=majority');
+    const db = client.db();
 
     if(req.method === 'POST'){
         const { email, name, text } = req.body;
@@ -9,11 +14,14 @@ const handler = (req,res) => {
         }
 
         const newComment = {
-            id: new Date().toISOString(),
             email,
             name,
-            text
-        }
+            text,
+            eventId
+        };
+
+        const result = await db.collection('comments').insertOne(newComment);
+        newComment.id = result.insertedId;
 
         return res.status(201).json({ msg: 'hello', comment: newComment });
 
@@ -26,6 +34,8 @@ const handler = (req,res) => {
 
         return res.status(200).json({ comments: dummyList });
     }
+
+    await client.close();
 }
 
 export default handler;
