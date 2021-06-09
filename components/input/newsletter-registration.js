14e67d0c2660ab7ce7a28e567559b26c4814666d
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-09 14:18:45 +0530

    add notifications to news letter

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,18 +1,47 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import axios from "axios";
 
 import classes from './newsletter-registration.module.css';
+import NotificationContext from "../../store/notification-context";
 
 function NewsletterRegistration() {
     const [email,setEmail] = useState('');
 
+    const notificationCtx = useContext(NotificationContext);
+
     async function registrationHandler(event) {
         event.preventDefault();
 
-        const res = await axios.post('http://localhost:3000/api/newsletter',{
-            email: email
+        notificationCtx.showNotification({
+            title: 'Sign up',
+            message: 'registering for newsletter',
+            status: 'pending'
         });
-        console.log(res.data);
+
+        try{
+
+            const res = await axios.post('http://localhost:3000/api/newsletter',{
+                email: email
+            });
+
+            if(res.status !== 201){
+                throw new Error('some thing error');
+            }
+
+            notificationCtx.showNotification({
+                title: 'Success',
+                message: 'registration success',
+                status: 'success'
+            });
+
+        }catch (err){
+
+            notificationCtx.showNotification({
+                title: 'Sign up error',
+                message: err.message || 'Something wrong',
+                status: 'error'
+            });
+        }
     }
 
     return (
