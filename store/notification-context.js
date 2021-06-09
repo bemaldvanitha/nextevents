0048393744304efcc2e346b4a0c034005cef9bfb
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-09 14:29:39 +0530

    notification cleanup automaticlly

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
     notification: null,
@@ -12,6 +12,20 @@ const NotificationContext = createContext({
 
 export const NotificationContextProvider = (props) => {
     const [activeNotification,setActiveNotification] = useState();
+
+    useState(() => {
+
+        if(activeNotification && activeNotification.status === 'success' || activeNotification.status === 'error'){
+
+            const timer = setTimeout(() => {
+                hideNotificationHandler();
+            },3000);
+
+            return () => {
+                clearTimeout(timer);
+            }
+        }
+    },[activeNotification]);
 
     const showNotificationHandler = (notificationData) => {
         setActiveNotification({
